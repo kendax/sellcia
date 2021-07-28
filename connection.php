224c38d9bdf4652becd9e6968a938afed987143a
kendax <munyivictor0@gmail.com>
2021-07-28 15:20:36 +0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -4,7 +4,6 @@ $serverName = "remotemysql.com";
 $userName = "egFfm6kqtF";
 $password = "KJKle4HN8d";
 $databaseName = "egFfm6kqtF";
-$port = "3306";
 
 $conn = mysqli_connect($dbconnection,$serverName,$userName,$password,$databaseName);
 
