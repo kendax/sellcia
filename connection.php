afb5b67406dc177c527c5e77e2dc25c84adc0ce5
kendax <munyivictor0@gmail.com>
2021-07-28 14:45:22 +0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,12 +1,12 @@
 <?php
-$dbconnection = "";
-$serverName = "";
-$userName = "";
-$password = "";
-$databaseName = "";
-$port = "";
+$dbconnection = "mysql";
+$serverName = "remotemysql.com";
+$userName = "egFfm6kqtF";
+$password = "KJKle4HN8d";
+$databaseName = "egFfm6kqtF";
+$port = "3306";
 
-$conn = mysqli_connect($serverName,$userName,$password,$databaseName,$port);
+$conn = mysqli_connect($dbconnection,$serverName,$userName,$password,$databaseName,$port);
 
 if(!$conn){
     echo "Disconnected";
