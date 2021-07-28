d8f9e4046300fa92cc870b6d051fc92b148a20da
kendax <munyivictor0@gmail.com>
2021-07-28 15:02:27 +0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -6,7 +6,7 @@ $password = "KJKle4HN8d";
 $databaseName = "egFfm6kqtF";
 $port = "3306";
 
-$conn = mysqli_connect($dbconnection,$serverName,$userName,$password,$databaseName,$port);
+$conn = mysqli_connect($dbconnection,$serverName,$userName,$password,$databaseName);
 
 if(!$conn){
     echo "Disconnected";
