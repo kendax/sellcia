f6d1c8cf486498218d9921b05b71a4d972b07f35
kendax <munyivictor0@gmail.com>
2021-07-29 15:02:26 +0300

    Update uploadvalidate.php

--- uploadvalidate.php
+++ uploadvalidate.php
@@ -10,7 +10,7 @@ if(isset($_POST['submit'])){
     $category = $_POST['category'];
     $description = $_POST['description'];
 
-    $currentDir = "https://kendax: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@github.com/kendax/sellcia";
+    $currentDir = "https://git.heroku.com/sellcia.git";
     $uploadDirectory = "/uploads/"; 
 
     $errors = [];
