64c5631410036b11cf0322427b6f53da1ab9eb5b
kendax <munyivictor0@gmail.com>
2021-08-01 11:09:07 +0300

    Update booksadmin.php

--- booksadmin.php
+++ booksadmin.php
@@ -159,7 +159,7 @@
                     <div class="img"><img src="https://firebasestorage.googleapis.com/v0/b/sellcia.appspot.com/o/uploads%2F<?php echo $rows1['Image'];?>?alt=media" alt="" style="max-height:150px; display:block; margin:auto;"></div>
                     <div class="product" style="text-align:center;"><?php echo $rows1['Product'];?></div>                 
                     <div class="price" style="text-align:center; font-weight:bold;">KSh <?php echo number_format($rows1['Cost']);?></div> 
-                    <input type="submit" value="BUY NOW" style="color:white; background-color:rgb(63, 170, 104); border:none; border-radius:5px; display:block; margin:auto;" border="0" name="submit" alt="">
+                    <input type="submit" value="BUY NOW" style="color:white; background-color:rgb(63, 170, 104); border:none; border-radius:5px; display:block; margin:auto; padding-bottom:none;" border="0" name="submit" alt="">
                 </a>
             </div>
             <div class="slot2">
