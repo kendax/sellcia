eb6ef1541a2cbe310ca7f5c32a6b2137d108a6ab
kendax <munyivictor0@gmail.com>
2021-08-03 14:43:17 +0300

    Update test.php

--- test.php
+++ test.php
@@ -79,7 +79,7 @@
     <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
 <link href="https://fonts.googleapis.com/css?family=Waiting+for+the+Sunrise" rel="stylesheet" type="text/css"/>
 </head>
-<body>
+<body class="body">
 <header>
 
 <meta name="viewport" content="width=device-width,initial-scale=1.0">
