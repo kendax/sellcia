7d43658ac00f4d10434e0c5100add002578bb4c3
kendax <munyivictor0@gmail.com>
2021-08-06 18:03:02 +0300

    Update test.php

--- test.php
+++ test.php
@@ -93,12 +93,12 @@
 	<ul class="menu" id="menu">
 		<li><div class="user-mobile" style="margin-left:-80px;">
          <p>Hi,Welcome</p>
-         </div style="transform:translateX(-50%);"></li>
+         </div></li>
          <link rel="stylesheet" href="css/searchbar.css">
          <link rel="stylesheet" href="https://img.icons8.com/pastel-glyph/64/000000/">
          <link rel="stylesheet" href="css/vela.css">
          
-            <li><a href="index.php" class="active"><img src="https://img.icons8.com/material-outlined/48/000000/home--v2.png" style="max-height:30px; transform:translateY(-19%);"/><span>    </span>Home</a></li>
+            <li style="transform:translateX(-50%);"><a href="index.php" class="active"><img src="https://img.icons8.com/material-outlined/48/000000/home--v2.png" style="max-height:30px; transform:translateY(-19%);"/><span>    </span>Home</a></li>
             <li><a href="electronics.php"><img src="https://img.icons8.com/ios/50/000000/tv-on.png" style="max-height:30px; transform:translateY(-19%);"/><span>    </span>Electronics</a></li>
             <li><a href="shoes.php"><img src="https://img.icons8.com/ios/50/000000/sneakers.png" style="max-height:30px; transform:translateY(-19%);"/><span>    </span>Shoes</a></li>
             <li><a href="phonesandtablets.php"><img src="https://img.icons8.com/dotty/80/000000/multiple-smartphones.png" style="max-height:30px; transform:translateY(-19%);"/><span>    </span>Phones and Tablets</a></li>
