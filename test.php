f672070aaaaa72b40d67435a1b08b4315a9f7fda
kendax <munyivictor0@gmail.com>
2021-08-10 23:25:57 +0000

    changes to test.php

--- test.php
+++ test.php
@@ -66,7 +66,7 @@
     <link rel="stylesheet" href="https://www.w3schools.com/lib/w3.css">
     <link href='https://fonts.googleapis.com/css?family=Abel' rel='stylesheet'>
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="css/vela.css">
+    <link rel="stylesheet" href="/css/vela.css">
     <!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
 
@@ -84,7 +84,7 @@
 <header>
 
 <meta name="viewport" content="width=device-width,initial-scale=1.0">
-<link rel="stylesheet" href="vela.css">
+<link rel="stylesheet" href="/css/vela.css">
 <script src="js/main.js"></script>
 
 <div id="container">
