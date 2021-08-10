6408ab3080c4d38c89fff87e98b3567367f244b7
kendax <munyivictor0@gmail.com>
2021-08-11 01:14:33 +0300

    Update test.php

--- test.php
+++ test.php
@@ -84,7 +84,7 @@
 <header>
 
 <meta name="viewport" content="width=device-width,initial-scale=1.0">
-<link rel="stylesheet" href="css/vela.css">
+<link rel="stylesheet" href="vela.css">
 <script src="js/main.js"></script>
 
 <div id="container">
